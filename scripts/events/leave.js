3603926e09b4e5302617dd40ac38eedef6329e1b
mohammad-kawsar-404 <mi1801773@gmail.com>
2024-05-18 08:31:41 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "꧌꧍᪥{name}᪥꧌꧍\n━━━━━━━━━━━━━━━━\n\nলিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator\n\n━━━━━━━━━━━━━━━━";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "꧌꧍᪥{name}᪥꧌꧍\n━━━━━━━━━━━━━━━━\n তুই Facebook users গ্রুপে থাকার যোগ্য না আবাল\n┏•━••━•━•━ ◎ ━•━•━••━•┓\n🔥 𝗚𝗢𝗢𝗗 𝗕𝗔𝗬 🔥\n┗•━•━••━•━ ◎ ━•━•━••━•┛ \n তর মত মেম্বার আমার BOSS এর দরকার নাই \n⊰᯽⊱┈────╌❊╌────┈⊰᯽⊱\n┏•━•━•━ ◎ ━•━•━•┓\n🔥 𝗕𝗢𝗧  𝗢𝗪𝗡𝗘𝗥🔥\n┗•━•━•━ ◎ ━•━•━•┛\n╭────────────────╮\n✜ 𝗠𝗢𝗛𝗔𝗠𝗠𝗔𝗗 𝗞𝗔𝗪𝗦𝗘𝗥 ✜\n╰────────────────╯. " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
